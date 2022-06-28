c94c173c1a5edb57663fcc857b66b2e544596b03
Igor Pershov <i.pershov@dunice.net>
2022-06-28 15:15:37 +0300

    add associations

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Post;
 
 class Tag extends Model
 {
     protected $fillable = ['tag'];
+
+    public function posts () {
+        return $this->belongsToMany(Post::class);
+    }
 }
