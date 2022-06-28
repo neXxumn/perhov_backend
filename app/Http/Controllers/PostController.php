57ca34f7accba5eaafd60f010e8e14c13c17f65d
Igor Pershov <i.pershov@dunice.net>
2022-06-28 09:42:21 +0300

    add use's

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Post;
+use Illuminate\Http\JsonResponse;
 
 class PostController extends Controller
 {
